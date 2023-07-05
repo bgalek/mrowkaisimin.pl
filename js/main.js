13af584cb98f8750306954387b4de6fcfc200448
Bartosz Gałek <bartosz@galek.com.pl>
2023-07-05 09:15:08 +0200

    first version

---
+++ js/main.js
@@ -0,0 +1,111 @@
+;(function () {
+
+    'use strict';
+
+    var dropdown = function () {
+
+        $('.has-dropdown').mouseenter(function () {
+
+            var $this = $(this);
+            $this
+                .find('.dropdown')
+                .css('display', 'block')
+                .addClass('animated-fast fadeInUpMenu');
+
+        }).mouseleave(function () {
+            var $this = $(this);
+
+            $this
+                .find('.dropdown')
+                .css('display', 'none')
+                .removeClass('animated-fast fadeInUpMenu');
+        });
+
+    };
+
+
+    var testimonialCarousel = function () {
+        var owl = $('.owl-carousel-fullwidth');
+        owl.owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 0,
+            responsiveClass: true,
+            nav: false,
+            dots: true,
+            smartSpeed: 800,
+            autoHeight: true,
+        });
+    };
+
+
+    var goToTop = function () {
+
+        $('.js-gotop').on('click', function (event) {
+
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $('html').offset().top
+            }, 500, 'easeInOutExpo');
+
+            return false;
+        });
+
+        $(window).scroll(function () {
+
+            var $win = $(window);
+            if ($win.scrollTop() > 200) {
+                $('.js-top').addClass('active');
+            } else {
+                $('.js-top').removeClass('active');
+            }
+
+        });
+
+    };
+
+
+    // Loading page
+    var loaderPage = function () {
+        $(".fh5co-loader").fadeOut("slow");
+    };
+
+    var counter = function () {
+        $('.js-counter').countTo({
+            formatter: function (value, options) {
+                return value.toFixed(options.decimals);
+            },
+        });
+    };
+
+    var counterWayPoint = function () {
+        if ($('#fh5co-counter').length > 0) {
+            $('#fh5co-counter').waypoint(function (direction) {
+
+                if (direction === 'down' && !$(this.element).hasClass('animated')) {
+                    setTimeout(counter, 400);
+                    $(this.element).addClass('animated');
+                }
+            }, {offset: '90%'});
+        }
+    };
+
+    // Parallax
+    var parallax = function () {
+        $(window).stellar();
+    };
+
+
+    $(function () {
+        parallax();
+        dropdown();
+        testimonialCarousel();
+        goToTop();
+        loaderPage();
+        counter();
+        counterWayPoint();
+    });
+
+
+}());
